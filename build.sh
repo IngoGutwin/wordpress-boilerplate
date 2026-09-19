85794f5f36c25376d7856106f8d4b1fa2dbc4c9f
Ingo Gutwin <onmabu@gmail.com>
2026-09-19 13:45:37 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -6,14 +6,8 @@ PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BUILD_DIR="$PROJECT_ROOT/build"
 THEME_DIR="$PROJECT_ROOT/wp/public/wp-content/themes/a_m_theme"
 
-install_dependencies() {
-    echo "intalling wordpress dependencies"
-    cd "$PROJECT_ROOT/wp"
-    composer install --no-dev --prefer-dist --optimize-autoloader
-    cd "$PROJECT_ROOT"
-}
-
 copy_plugin_files() {
+    echo "copy plugins"
     cp -r "wp/public/wp-content/mu-plugins" "$BUILD_DIR/public/wp-content"
     cp -r "wp/public/wp-content/plugins" "$BUILD_DIR/public/wp-content"
 }
@@ -31,7 +25,8 @@ copy_wp_files() {
     cp "wp/app.php" "$BUILD_DIR"
     cp "wp/public/index.php" "$BUILD_DIR/public/"
     cp "wp/public/wp-config.php" "$BUILD_DIR/public/" 
-    cp -r "wp/public/wordpress" "$BUILD_DIR/public"
+    cp -r "wp/public/wordpress" "$BUILD_DIR/public/"
+    cp -r "wp/vendor" "$BUILD_DIR/"
 }
 
 create_folder_structur() {
@@ -54,6 +49,7 @@ run_script() {
     create_folder_structur
     copy_wp_files
     build_theme
+    copy_plugin_files
 }
 
 if [ -d "$BUILD_DIR" ]; then
